fe016910fc4a471d26d0d97e3e07767f82bece20
AnastasiyaKononova <anastkononova@gmail.com>
2016-05-17 22:38:40 +0300

    Задание 2

---
+++ L7_2/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Labor7_2
+//
+//  Created by Admin on 17.05.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
